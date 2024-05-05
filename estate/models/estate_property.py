b2acb2b1bf36b936a03aaec7d597ab1bea218c0d
Gabriel Sanhueza Fuentes <gsanhuezafuentes11@gmail.com>
2024-05-04 23:17:05 -0400

    complete until 10 tutorials

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,6 +1,8 @@
 from datetime import timedelta
 
 from odoo import api, models, fields
+from odoo.exceptions import UserError, ValidationError
+from odoo.tools import float_compare, float_is_zero
 
 class EstateProperty(models.Model):
     _name = 'estate.property'
@@ -28,7 +30,7 @@ class EstateProperty(models.Model):
     )
     last_seen = fields.Datetime("Last Seen", default=fields.Datetime.now)
     active = fields.Boolean(default=True)
-    state = fields.Selection(
+    status = fields.Selection(
         selection=[
             ('new', 'New'),
             ('offer_received', 'Offer Received'),
@@ -44,6 +46,11 @@ class EstateProperty(models.Model):
     offers_ids = fields.One2many('estate.property.offer', 'property_id')
     total_area = fields.Float(compute="_compute_total_area")
     best_price = fields.Float(compute="_compute_best_price")
+
+    _sql_constraints = [
+        ("check_expected_price", "CHECK(expected_price > 0)", "The expected price must be strictly positive"),
+        ("check_selling_price", "CHECK(selling_price >= 0)", "The selling price must be positive"),
+    ]
 
     @api.depends('living_area', 'garden_area')
     def _compute_total_area(self):
@@ -65,10 +72,29 @@ class EstateProperty(models.Model):
             self.garden_area = 0
             self.garden_orientation = ''
     
-
+    def action_sold(self):
+        for record in self:
+            if record.status == 'canceled':
+                raise UserError("Can't mark as sold a canceled property")
+            record.status = 'sold'
+        return True
     
-
-
+    def action_cancel(self):
+        for record in self:
+            if record.status == 'sold':
+                raise UserError("Can't cancel a sold property")
+            record.status = 'canceled'
+        return True
+    
+    @api.constrains('selling_price', 'expected_price')
+    def _check_selling_price(self):
+        for record in self:
+            if (
+                not float_is_zero(record.selling_price, 2) 
+                and float_compare(record.selling_price, record.expected_price * .9) < 0
+            ):
+                raise ValidationError("The selling price can't be lower than 90% of the expected price")
+      
 class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
     _description = 'Estate Property Type'
@@ -80,7 +106,11 @@ class EstatePropertyTag(models.Model):
     _name = 'estate.property.tag'
     _description = 'Estate Property Tag'
 
-    name = fields.Char(required=True)
+    name = fields.Char(required=True, unique=True)
+
+    _sql_constraints = [
+        ("unique_name", "UNIQUE(name)", "The name must be unique"),
+    ]
 
 
 class EstatePropertyOffer(models.Model):
@@ -101,6 +131,10 @@ class EstatePropertyOffer(models.Model):
     partner_id = fields.Many2one('res.partner')
     property_id = fields.Many2one('estate.property')
 
+    _sql_constraints = [
+        ("check_price", "CHECK(price > 0)", "The price must be strictly positive"),
+    ]
+
     @api.depends('create_date', 'validity')
     def _compute_date_deadline(self):
         for record in self:
@@ -110,4 +144,16 @@ class EstatePropertyOffer(models.Model):
     def _inverse_date_deadline(self):
         for record in self:
             record.validity = (record.date_deadline - record.create_date.date()).days
+    
+    def accept_offer(self):
+        for record in self:
+            record.property_id.selling_price = record.price
+            record.property_id.buyer_id = record.partner_id
+            record.status = 'accepted'
+        return True
 
+    def refuse_offer(self):
+        for record in self:
+            record.status = 'refused'
+
+        return True
